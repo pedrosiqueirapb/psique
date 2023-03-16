619f64d7e4f05a354d8557d6d008efa873e0f422
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2023-03-16 17:20:22 -0300

    Testando carrossel

--- src/telaPrincipal.js
+++ src/telaPrincipal.js
@@ -2,14 +2,14 @@ $(document).ready(()=>{
     $('.temaClaro').hide()
 
     $('.temaEscuro').click(()=>{
-        $('#corpoSite').css('background-color', '#121314')
+        $('body').css('background-color', '#121314')
         $('h1').css('color', '#FFF')
         $('.temaEscuro').hide()
         $('.temaClaro').show()
     })
 
     $('.temaClaro i').click(()=>{
-        $('#corpoSite').css('background-color', '#f7f7f7')
+        $('body').css('background-color', '#f7f7f7')
         $('h1').css('color', '#000')
         $('.temaEscuro').show()
         $('.temaClaro').hide()
