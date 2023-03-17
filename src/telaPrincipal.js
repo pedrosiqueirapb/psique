6e99b1bbc2ed5ab68930f6e03cf59e2b5b444aed
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2023-03-16 21:53:53 -0300

    Botões 'Mostrar Mais' e 'Mostrar Menos'

--- src/telaPrincipal.js
+++ src/telaPrincipal.js
@@ -1,8 +1,9 @@
 $(document).ready(()=>{
+    $('body').css('background-color', '#f7f7f7')
     $('.temaClaro').hide()
 
     $('.temaEscuro').click(()=>{
-        $('body').css('background-color', '#121314')
+        $('body').css('background-color', '#171a21')
         $('h1').css('color', '#FFF')
         $('.temaEscuro').hide()
         $('.temaClaro').show()
@@ -13,5 +14,17 @@ $(document).ready(()=>{
         $('h1').css('color', '#000')
         $('.temaEscuro').show()
         $('.temaClaro').hide()
+    })
+
+    $('#btn-Mais').click(()=>{
+        $('.psicologos .container > .infosPsic:last-child').css('display', 'flex')
+        $('#btn-Mais').hide()
+        $('#btn-Menos').show()
+    })
+
+    $('#btn-Menos').click(()=>{
+        $('.psicologos .container > .infosPsic:last-child').css('display', 'none')
+        $('#btn-Mais').show()
+        $('#btn-Menos').hide()
     })
 })
