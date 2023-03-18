2759c696d5b5a6e7515635a1d9e9ef5964346e0e
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2023-03-18 16:17:49 -0300

    Concluindo algumas seções do site

--- src/telaPrincipal.js
+++ src/telaPrincipal.js
@@ -1,31 +1,42 @@
 $(document).ready(()=>{
-    $('body').css('background-color', '#f7f7f7')
+    $('body').css('background-color', '#f3f2f2')
+
     $('.temaClaro').hide()
 
     $('.temaEscuro').click(()=>{
         $('body').css('background-color', '#171a21')
-        $('h1, #descricao').css('color', '#FFF')
-        $('#logotipo').css('border-color', '#f7f7f7')
+        $('h1, h3, main p').css('color', '#FFF')
+        $('#logotipo, #imagemIA').css('border-color', '#f3f2f2')
+
+        $('.modoClaro').hide()
+        $('.modoEscuro').show()
+
         $('.temaEscuro').hide()
         $('.temaClaro').show()
     })
 
     $('.temaClaro i').click(()=>{
-        $('body').css('background-color', '#f7f7f7')
-        $('h1, #descricao').css('color', '#000')
-        $('#logotipo').css('border-color', '#171a21')
+        $('body').css('background-color', '#f3f2f2')
+        $('h1, h3, main p').css('color', '#000')
+        $('#logotipo, #imagemIA').css('border-color', '#171a21')
+
+        $('.modoEscuro').hide()
+        $('.modoClaro').show()
+
         $('.temaEscuro').show()
         $('.temaClaro').hide()
     })
 
     $('#btn-Mais').click(()=>{
         $('.psicologos .container > .infosPsic:last-child').css('display', 'flex')
+
         $('#btn-Mais').hide()
         $('#btn-Menos').show()
     })
 
     $('#btn-Menos').click(()=>{
         $('.psicologos .container > .infosPsic:last-child').css('display', 'none')
+
         $('#btn-Mais').show()
         $('#btn-Menos').hide()
     })
